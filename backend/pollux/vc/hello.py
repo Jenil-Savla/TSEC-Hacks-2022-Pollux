29406865b646edf64d77f34b96565ca0296964c5
Jenil-Savla <you@example.com>
2022-03-10 07:17:03 +0530

    Debug

--- backend/pollux/vc/hello.py
+++
@@ -1,9 +0,0 @@
-from django_zoom_meetings import ZoomMeetings
-
-import environ
-
-env = environ.Env()
-environ.Env.read_env()
-
-my_zoom = ZoomMeetings(env('ZOOM_API_KEY'), env('ZOOM_API_SECRET'))
-create_meeting = my_zoom.CreateMeeting(date,str_topic,str_meeting_duration,str_meeting_password)
